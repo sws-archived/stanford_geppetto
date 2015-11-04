7defe3619f29d51df9647a76a1dd844c140ca55e
Shea McKinney <sherakama@gmail.com>
2015-11-03 22:47:36 -0800

    Added post install to init composer_manager

--- scripts/composer/post-install.sh
+++ scripts/composer/post-install.sh
@@ -1,1 +1,5 @@
-echo "You're done for now"
+# Friendly messages are fun.
+echo "Site has been successfully built using the friendly SWS Geppetto"
+
+# Initialize composer manager:
+php web/modules/contrib/composer_manager/scripts/init.php
