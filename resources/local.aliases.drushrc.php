a0eed966fa596ca6e338f78c4aa58f24bc3c4947
Shea McKinney <sherakama@gmail.com>
2015-12-04 11:45:32 -0800

    Updated local site aliases resource

--- resources/local.aliases.drushrc.php
+++ resources/local.aliases.drushrc.php
@@ -22,8 +22,6 @@ foreach ($command as $arg) {
   if ($last == "dev") {
     // Set the project to be whatever the alias was.
     $project = str_replace('@', '', $arg);
-    // Remove the .dev tacked on thingymagig.
-    $project = str_replace(".dev", "", $project);
   }
 
 }
@@ -31,12 +29,16 @@ foreach ($command as $arg) {
 // Only act if we have something.
 if (!is_null($project)) {
 
+  // Remove the .dev tacked on thingymagig.
+  $dir_name = str_replace(".dev", "", $project);
+
   // This will be project.dev.
   $aliases[$project] = array(
     // Where the local Drupal root is at. Something like /var/www/jsv.
-    'root' => $local_sites . $project,
+    'root' => $local_sites . $dir_name,
     'path-aliases' => array(
       '%dump-dir' => '/tmp',
     )
   );
+
 }
